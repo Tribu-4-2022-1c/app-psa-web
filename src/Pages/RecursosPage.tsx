a1219b00cc2a44a2a7ca1bcfdc641e878a1728af
fedepenic <fedepenic@hotmail.com>
2022-06-22 22:20:16 -0300

    Add Loader Spinner

--- src/Pages/RecursosPage.tsx
+++ src/Pages/RecursosPage.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
+import { Audio } from 'react-loader-spinner';
+
+import { useEffect, useState } from 'react';
+
+import recursosCSS from '../Styles/Recursos.module.css';
+
 const RecursosPage = () => {
+
+  const [load, setload] = useState(true);
+
+  const winHeight =  window.innerHeight*.8;
+
   return (
-    <div>RecursosPage</div>
+    <div >
+      {load&&<div style={{height: winHeight}} className={`${recursosCSS.contentAudio}`}>
+          <Audio
+          height="50"
+          width="50"
+          color='#003066'
+          ariaLabel='loading'
+        />
+        </div>}
+    </div >
   )
 }
 
