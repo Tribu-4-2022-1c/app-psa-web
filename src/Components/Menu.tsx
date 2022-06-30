ec599c2a62bfdc7b6f27d162788f5ce0c4154cbc
RoggerParedesTavara <rparedestavara@gmail.com>
2022-06-30 20:08:24 -0300

    Prueba

--- src/Components/Menu.tsx
+++ src/Components/Menu.tsx
@@ -1,6 +1,8 @@
+import React from 'react';
+
 import menuModuleCSS from '../Styles/Menu.module.css';
 import img from '../assets/psa_icon.png';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 const Menu = () => {
     const routePSA = {
@@ -12,14 +14,19 @@ const Menu = () => {
         nameRoute: '/proyectos',
         isSelected: false,
     }
+    const { pathname } = useLocation();
     const [pageSelected, setpageSelected] = useState(intialStatePageSelected);
-    // useEffect(() => {
-    //     setpageSelected({...pageSelected,nameRoute:routePSA.proyectos});
-    // }, )
 
     const changePage = (route:string) => {
-        setpageSelected({...pageSelected,nameRoute:route});
+        setpageSelected({...pageSelected,nameRoute:route,isSelected:true});
     }
+
+    useEffect(() => {
+        let path = (pathname.split('/').length>1)?pathname.split('/')[1]:'';
+        path = '/'+path;
+        console.log(path);
+        changePage(path);
+    },[]);
 
     return (
         <div className={menuModuleCSS.contentMenu}>
@@ -27,45 +34,22 @@ const Menu = () => {
                 <img className={menuModuleCSS.imagePSA} src={img} alt='\' />
                 <p>Praxis Systems Argentina</p>
             </div>
-             {/* <div className={menuModuleCSS.contentButton}>
-               <NavLink className={menuModuleCSS.linkEfect} to={routePSA.proyectos} onClick={() => changePage(routePSA.proyectos)}>
-                    <div className={menuModuleCSS.buttonSection}>
-                        <p>PROYECTOS</p>
-                        <hr className={pageSelected.nameRoute === routePSA.proyectos ? menuModuleCSS.line : menuModuleCSS.notLine} />
-                    </div>
-                </NavLink>
-                <NavLink className={menuModuleCSS.linkEfect} to={routePSA.soporte} onClick={() => changePage(routePSA.soporte)}>
-                    <div className={menuModuleCSS.buttonSection}>
-                        <p>SOPORTES</p>
-                        <hr className={pageSelected.nameRoute === routePSA.soporte ? menuModuleCSS.line : menuModuleCSS.notLine} />
-                    </div>
-                </NavLink>
-                <NavLink className={menuModuleCSS.linkEfect} to={routePSA.recursos} onClick={() => changePage(routePSA.recursos)}>
-                    <div className={menuModuleCSS.buttonSection}>
-                        <p>RECURSOS</p>
-                        <hr className={pageSelected.nameRoute === routePSA.recursos ? menuModuleCSS.line : menuModuleCSS.notLine} />
-                    </div>
-                </NavLink>
-            </div> */}
             <div className={menuModuleCSS.contentSegmentMenu}>
-                <div className={menuModuleCSS.optionSegment}>
+                <div className={`${menuModuleCSS.optionSegment} ${(pageSelected.nameRoute === routePSA.proyectos || pageSelected.nameRoute === '/')?menuModuleCSS.addMarker:''}`}>
                     <NavLink end className={menuModuleCSS.linkEfect} to={routePSA.proyectos} onClick={() => changePage(routePSA.proyectos)}>
                         PROYECTOS
                     </NavLink>
-                    <hr className={pageSelected.nameRoute === routePSA.proyectos ? menuModuleCSS.line : menuModuleCSS.notLine} />
                 </div>
-                <div className={menuModuleCSS.optionSegment}>
+                <div className={`${menuModuleCSS.optionSegment} ${(pageSelected.nameRoute === routePSA.soporte)?menuModuleCSS.addMarker:''}`}>
                     <NavLink end className={menuModuleCSS.linkEfect} 
                     to={routePSA.soporte} onClick={() => changePage(routePSA.soporte)}>
                         SOPORTES
                     </NavLink>
-                    <hr className={pageSelected.nameRoute === routePSA.soporte ? menuModuleCSS.line : menuModuleCSS.notLine} />
                 </div>
-                <div className={menuModuleCSS.optionSegment}>
+                <div className={`${menuModuleCSS.optionSegment} ${pageSelected.nameRoute === routePSA.recursos?menuModuleCSS.addMarker:''}`}>
                     <NavLink end className={menuModuleCSS.linkEfect} to={routePSA.recursos} onClick={() => changePage(routePSA.recursos)}>
                         RECURSOS
                     </NavLink>
-                    <hr className={pageSelected.nameRoute === routePSA.recursos ? menuModuleCSS.line : menuModuleCSS.notLine} />
                 </div>
             </div>
         </div>
