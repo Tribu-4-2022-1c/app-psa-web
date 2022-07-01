9f30d4133068f0b8f31ca83b93a6f8d20c5b5c55
RoggerParedesTavara <rparedestavara@gmail.com>
2022-07-01 02:09:51 -0300

    2 colunas funciona heroku

--- src/Components/Soporte/DetalleTicket.tsx
+++ src/Components/Soporte/DetalleTicket.tsx
@@ -113,8 +113,8 @@ export const DetalleTicket = () => {
       <MenuDescription version={version} product={product} flagGenerateTask={flagGenerateTask} functionGenerateTask={generateTask}/>
       {/* <ModalComponent show={show} employees={employees} closeModal={closeModal} agregarTarea={agregarTarea}/>   */}
           <Row className={detalleTicketCSS.contentRow}>
-             {/*<Col className={detalleTicketCSS.col4} md={6} lg={6} m={6}>
-              <Row>
+             <Col className={detalleTicketCSS.col4} md={6}>
+              {/*<Row>
                 <div className={detalleTicketCSS.contentItem}>
                   <Form.Label className={detalleTicketCSS.label}>Cliente:</Form.Label>
                   <Form.Control
@@ -212,10 +212,10 @@ export const DetalleTicket = () => {
                     </div>
                   </div>
                 </Col>
-              </Row>
+              </Row>*/}
             </Col>
-            <Col className={detalleTicketCSS.col8} md={6} lg={6} m={6}>
-              {!disabled&&<Button className={detalleTicketCSS.iconSave} onClick={() => updateData()} variant="success">Guardar</Button>}
+            <Col className={detalleTicketCSS.col8} md={6}>
+              {/* {!disabled&&<Button className={detalleTicketCSS.iconSave} onClick={() => updateData()} variant="success">Guardar</Button>}
               {!disabled&&<MdHighlightOff className={`${detalleTicketCSS.editIcon} ${detalleTicketCSS.iconClose}`} onClick={() => changeStateEdit(true)} />}
               {disabled&&<FaEdit className={`${detalleTicketCSS.editIcon}`} onClick={() => changeStateEdit(false)} />}
               <Row>
@@ -245,8 +245,8 @@ export const DetalleTicket = () => {
                   />
                 </div>
                 </Col>
-              </Row>
-            </Col> */}
+              </Row> */}
+            </Col>
           </Row> 
       {ticket.type!=='CONSULTA'&&<div className={`${detalleTicketCSS.contentTaskTickets} ${(task&&task.length===0)?detalleTicketCSS.uninformation:''}`}>
        {(task&&task.length>0)&&<div><Table responsive bordered >
