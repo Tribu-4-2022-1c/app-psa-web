19c4a45769eaefa32c89cc6ef2a3d283770a1280
RoggerParedesTavara <rparedestavara@gmail.com>
2022-07-01 19:39:56 -0300

    Se agrega el Modal

--- src/Components/Soporte/TicketDetalle.tsx
+++ src/Components/Soporte/TicketDetalle.tsx
@@ -1,20 +1,31 @@
 import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom';
+import { ModalComponent } from '../ModalComponent';
 import MenuDescription from './MenuDescription'
 
-export const TicketDetalle = (props:any) => {
+export const TicketDetalle = (props: any) => {
     const location = useLocation();
     const { ticket, version, product, severities, employees }: any = location.state;
     const [flagGenerateTask, setflagGenerateTask] = useState(false);
     const [show, setshow] = useState(false);
+    const [task, setTask] = useState<any[]>([]);
 
     const generateTask = () => {
         setshow(true);
-      }
+    }
 
-  return (
-    <div>
-        <MenuDescription version={version} product={product} flagGenerateTask={flagGenerateTask} functionGenerateTask={generateTask} />
-    </div>
-  )
+    const closeModal = () => {
+        setshow(false);
+    }
+    const agregarTarea = (newTask: any) => {
+        task.push(newTask);
+        setshow(false);
+    }
+
+    return (
+        <div>
+            <MenuDescription version={version} product={product} flagGenerateTask={flagGenerateTask} functionGenerateTask={generateTask} />
+            <ModalComponent show={show} employees={employees} closeModal={closeModal} agregarTarea={agregarTarea}></ModalComponent>
+        </div>
+    )
 }
