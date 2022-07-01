6566ccc925a85d8fc6fd3a21675c585e3b37e649
RoggerParedesTavara <rparedestavara@gmail.com>
2022-06-30 23:04:03 -0300

    Prueba 4

--- src/Components/Soporte/DetalleTicket.tsx
+++ src/Components/Soporte/DetalleTicket.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+/*import React, { useCallback, useEffect, useState } from "react";
 import { FaCalendar, FaQuestionCircle, FaEdit, FaEye } from 'react-icons/fa';
 import { MdOutlineError, MdTipsAndUpdates,MdHighlightOff } from "react-icons/md";
 import { Button, Card, Col, Form, Row, Table } from 'react-bootstrap';
@@ -283,4 +283,12 @@ export const DetalleTicket = () => {
       </div>}
     </div>
   )
+}*/
+import React from 'react'
+
+export const DetalleTicket = () => {
+  return (
+    <div>DetalleTicket</div>
+  )
 }
+
