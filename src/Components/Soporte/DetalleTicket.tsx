9369f94cd2249141052b9c62b900f078f91d8e98
RoggerParedesTavara <rparedestavara@gmail.com>
2022-07-01 00:47:43 -0300

    Se quita una columna

--- src/Components/Soporte/DetalleTicket.tsx
+++ src/Components/Soporte/DetalleTicket.tsx
@@ -114,7 +114,7 @@ export const DetalleTicket = () => {
       <ModalComponent show={show} employees={employees} closeModal={closeModal} agregarTarea={agregarTarea}/>
       <div>
         <Row className={detalleTicketCSS.contentRow}>
-          <Col className={detalleTicketCSS.col4} md={6} lg={6} m={6}>
+          {/* <Col className={detalleTicketCSS.col4} md={6} lg={6} m={6}>
             <Row>
               <div className={detalleTicketCSS.contentItem}>
                 <Form.Label className={detalleTicketCSS.label}>Cliente:</Form.Label>
@@ -214,12 +214,12 @@ export const DetalleTicket = () => {
                 </div>
               </Col>
             </Row>
-          </Col>
+          </Col> */}
           <Col className={detalleTicketCSS.col8} md={6} lg={6} m={6}>
             {!disabled&&<Button className={detalleTicketCSS.iconSave} onClick={() => updateData()} variant="success">Guardar</Button>}
             {!disabled&&<MdHighlightOff className={`${detalleTicketCSS.editIcon} ${detalleTicketCSS.iconClose}`} onClick={() => changeStateEdit(true)} />}
             {disabled&&<FaEdit className={`${detalleTicketCSS.editIcon}`} onClick={() => changeStateEdit(false)} />}
-            <Row>
+            {/* <Row>
               <Col>
               <div>
                 <Form.Label className={detalleTicketCSS.label} htmlFor="title">Título:</Form.Label>
@@ -246,7 +246,7 @@ export const DetalleTicket = () => {
                 />
               </div>
               </Col>
-            </Row>
+            </Row> */}
           </Col>
         </Row>
       </div>
