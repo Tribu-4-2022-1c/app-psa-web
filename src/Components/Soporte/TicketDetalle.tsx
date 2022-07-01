271771ed1240fff8d6ed81c5cca7357ca6fa6be8
RoggerParedesTavara <rparedestavara@gmail.com>
2022-07-01 20:03:10 -0300

    Condicionales

--- src/Components/Soporte/TicketDetalle.tsx
+++ src/Components/Soporte/TicketDetalle.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react'
+import moment from 'moment';
+import React, { useCallback, useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom';
+import { Ticket } from '../../models/Soporte.models';
 import { ModalComponent } from '../ModalComponent';
 import MenuDescription from './MenuDescription'
+import soporteService from "../../Services/soporteService";
 
 export const TicketDetalle = (props: any) => {
     const location = useLocation();
@@ -9,11 +12,32 @@ export const TicketDetalle = (props: any) => {
     const [flagGenerateTask, setflagGenerateTask] = useState(false);
     const [show, setshow] = useState(false);
     const [task, setTask] = useState<any[]>([]);
-
+    const [diasRestantes, setdiasRestantes] = useState(0);
+    const initialTicket: Ticket = {
+        code: 0,
+        title: '',
+        description: '',
+        type: '',
+        client: '',
+        version: '',
+        severity: '',
+        status: '',
+        creationDate: '',
+        lastUpdated: '',
+        closureMotive: null,
+        resolution: ''
+      }
+    const [ticketCurrent, setticketCurrent] = useState(initialTicket);
     const generateTask = () => {
         setshow(true);
     }
 
+    const validateFlagGenerateTask = useCallback(() => {
+        if (ticket.type !== 'CONSULTA') {
+          setflagGenerateTask(true);
+        }
+      }, [ticket])
+      
     const closeModal = () => {
         setshow(false);
     }
@@ -21,6 +45,29 @@ export const TicketDetalle = (props: any) => {
         task.push(newTask);
         setshow(false);
     }
+
+    const getDiasDeVencimiento = useCallback((severity: string, dateCreation: string) => {
+        let fecha1 = moment(dateCreation);
+        let fecha2 = moment();
+        let optionSev = severities.find((x: any) => x.level === severity);
+        let diffDate = fecha2.diff(fecha1, 'days');
+        setdiasRestantes(optionSev.days - diffDate);
+      }, [severities])
+
+      const getTicketsTask = async () => {
+        let taskArray: any = await soporteService().getTicketsTask();
+        setTask(taskArray);
+      }
+
+    useEffect(() => {
+        const getData = async () => {
+          setticketCurrent({ ...ticket });
+          getDiasDeVencimiento(ticket.severity, ticket.creationDate);
+          await getTicketsTask();
+          validateFlagGenerateTask();
+        }
+        getData();
+      }, [ticket, getDiasDeVencimiento, validateFlagGenerateTask])
 
     return (
         <div>
