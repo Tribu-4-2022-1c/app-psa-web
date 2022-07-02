7065d5bc7b2d9f35dca8f01ee69ad97e25261867
RoggerParedesTavara <rparedestavara@gmail.com>
2022-07-01 22:46:00 -0300

    tag P

--- src/Components/Soporte/TicketDetalle.tsx
+++ src/Components/Soporte/TicketDetalle.tsx
@@ -177,11 +177,11 @@ export const TicketDetalle = (props: any) => {
                     </Col>
                     <Col className={detalleTicketCSS.col8} md={6}>
                         {/* {!disabled && <>
-                            <Button className={detalleTicketCSS.iconSave} onClick={() => updateData()} variant="success">Guardar</Button>
+                            < className={detalleTicketCSS.iconSave} onClick={() => updateData()} variant="success">Guardar</Button>
                             <MdHighlightOff className={`${detalleTicketCSS.editIcon} ${detalleTicketCSS.iconClose}`} onClick={() => changeStateEdit(true)} />
                         </>
-                        }
-                        {disabled && <FaEdit className={`${detalleTicketCSS.editIcon}`} onClick={() => changeStateEdit(false)} />} */}
+                        } */}
+                        {disabled && <p><FaEdit className={`${detalleTicketCSS.editIcon}`} onClick={() => changeStateEdit(false)} /></p>}
                         <Form.Group className={detalleTicketCSS.contentItem}>
                             <Form.Label className={detalleTicketCSS.label} htmlFor="title">Título:</Form.Label>
                             <Form.Control
