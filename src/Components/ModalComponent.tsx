4d68a94ead5cfd00aa497885a430a05d0d78eca2
RoggerParedesTavara <rparedestavara@gmail.com>
2022-07-02 03:47:51 -0300

    Limpieza de Código

--- src/Components/ModalComponent.tsx
+++ src/Components/ModalComponent.tsx
@@ -12,6 +12,7 @@ export const ModalComponent = (props: any) => {
     const [newTarea, setnewTarea] = useState(initialTaskData);
 
     const agregarTareaNueva = () => {
+        console.log(newTarea)
         agregarTarea(newTarea);
     }
 
@@ -49,47 +50,11 @@ export const ModalComponent = (props: any) => {
                 <Button variant="secondary" onClick={closeModal}>
                     Close
                 </Button>
-                <Button variant="primary" onClick={agregarTarea}>
+                <Button variant="primary" onClick={() => agregarTareaNueva()}>
                     Agregar
                 </Button>
                 </Modal.Footer>
             </Modal>
         </div>
-            // <Modal
-            //     show={show}
-            //     aria-labelledby="contained-modal-title-vcenter"
-            //     centered
-            // >
-            //     <ModalHeader>
-            //         <Modal.Title className={modalCSS.titleTarea}>Nueva Tarea</Modal.Title>
-            //         <IoCloseCircleSharp onClick={() => closeModal()} className={modalCSS.iconClose}/>
-            //     </ModalHeader>
-            //     <Modal.Body>
-
-            //         <FloatingLabel controlId="floatingSelectGrid" label="Works with selects" className={modalCSS.labelSelect}>
-            //             <Form.Select placeholder="Seleccionar Encargado" onChange={(value) => changeState('employeed',value)}>
-            //                 {employees && employees.length > 0 && employees.map((x: any, index: number) =>
-            //                     <option key={x.legajo}>{x.nombre + ' ' + x.apellido}</option>
-            //                 )}
-            //             </Form.Select>
-            //         </FloatingLabel>
-
-            //         <FloatingLabel controlId="floatingTextarea2" label="Titulo" className={modalCSS.labelSelect}>
-            //             <Form.Control
-            //                 as="textarea"
-            //                 placeholder="Titulo"
-            //                 rows={3}
-            //                 style={{ height: '100px' }}
-            //                 onChange={(value) => changeState('title',value)}
-            //             />
-            //         </FloatingLabel>
-                    
-            //     </Modal.Body>
-            //     <Modal.Footer>
-            //         <Button onClick={() => agregarTareaNueva()}>
-            //             Agregar
-            //         </Button>
-            //     </Modal.Footer>
-            // </Modal>
     )
 }
--- src/Components/Soporte/TicketDetalle.tsx
+++ src/Components/Soporte/TicketDetalle.tsx
@@ -6,11 +6,9 @@ import { ModalComponent } from '../ModalComponent';
 import MenuDescription from './MenuDescription'
 import soporteService from "../../Services/soporteService";
 import detalleTicketCSS from '../../Styles/Detalle.module.css';
-import { FaCalendar, FaEdit, FaEye, FaQuestionCircle, FaProductHunt } from 'react-icons/fa';
-import { MdOutlineError, MdTipsAndUpdates, MdHighlightOff } from "react-icons/md";
+import { FaCalendar, FaEdit, FaEye } from 'react-icons/fa';
 import { Button, Card, Col, Container, Form, InputGroup, Row, Table } from 'react-bootstrap';
 import CardHeader from 'react-bootstrap/esm/CardHeader';
-import { IoCloseCircle, IoPencilSharp } from "react-icons/io5";
 import { IoIosClose } from "react-icons/io";
 
 export const TicketDetalle = (props: any) => {
@@ -65,6 +63,7 @@ export const TicketDetalle = (props: any) => {
         setshow(false);
     }
     const agregarTarea = (newTask: any) => {
+        console.log(newTask.data)
         task.push(newTask);
         setshow(false);
     }
@@ -210,27 +209,13 @@ export const TicketDetalle = (props: any) => {
                         </div>
                     </Col>
                     <Col className={detalleTicketCSS.col8} md={6}>
-                        {/* <div>{!disabled && (<>
-                            <Button className={detalleTicketCSS.iconSave} onClick={() => updateData()} variant="success">Guardar</Button>
-                            <p><MdHighlightOff className={`${detalleTicketCSS.editIcon} ${detalleTicketCSS.iconClose}`} onClick={() => changeStateEdit(true)} /></p>
-                        </>)
-                        }</div> */}
-                        {/* <FaEdit/> */}
                         {!disabled && <Button className={detalleTicketCSS.iconSave} onClick={() => updateData()} variant="success">Guardar</Button>}
-                        {/* {!disabled &&  <Button className={`${detalleTicketCSS.editIcon} ${detalleTicketCSS.iconClose}`} onClick={() => changeStateEdit(true)}>
-                            <FaProductHunt />
-                        </Button>} */}
                         {!disabled &&
                             <IoIosClose className={`${detalleTicketCSS.editIcon} ${detalleTicketCSS.iconClose}`} onClick={() => changeStateEdit(true)} />
                         }
                         {disabled &&
                             <FaEdit className={`${detalleTicketCSS.editIcon}`} onClick={() => changeStateEdit(false)} />
                         }
-                        {/* <Button> </Button> */}
-                        {/* <MdHighlightOff /> */}
-                        {/* {disabled && <div><FaEdit className={`${detalleTicketCSS.editIcon}`} onClick={() => changeStateEdit(false)} /></div>}
-
-                        {!disabled && <div><MdHighlightOff className={`${detalleTicketCSS.editIcon} ${detalleTicketCSS.iconClose}`} onClick={() => changeStateEdit(true)} /></div>} */}
                         <Form.Group className={detalleTicketCSS.contentItem}>
                             <Form.Label className={detalleTicketCSS.label} htmlFor="title">Título:</Form.Label>
                             <Form.Control
@@ -258,144 +243,7 @@ export const TicketDetalle = (props: any) => {
                     </Col>
                 </Row>
             </Container>
-            {/* <Container>
-            <Row className={detalleTicketCSS.contentRow}>
-        <Col className={detalleTicketCSS.col4} md={6}>
-           <Row>
-            <div className={detalleTicketCSS.contentItem}>
-                  <Form.Label className={detalleTicketCSS.label}>Cliente:</Form.Label>
-                  <Form.Control
-                    className={`${(disabled) ? detalleTicketCSS.disabled : ''} ${detalleTicketCSS.input}`}
-                    type="text"
-                    id="client"
-                    disabled={disabled}
-                    value={ticketCurrent.client}
-                    onChange = {(value) => changeValue('client',value)}
-                  />
-                </div>
-                <div className={detalleTicketCSS.contentItem}>
-                    <Form.Label className={detalleTicketCSS.label}>Responsable:</Form.Label>
-                    <div className={detalleTicketCSS.contentInput}>
-                      <Form.Select value={ticketCurrent.status} disabled={disabled} className={`${(disabled) ? detalleTicketCSS.disabled : ''} 
-                      ${detalleTicketCSS.input} ${detalleTicketCSS.addRightSelect}`} onChange={(value) => changeValue('status',value)}>
-                        {employees.map((type: any, index: number) => <option key={type.legajo} value={type}>{type.nombre}  {type.apellido}</option>)}
-                      </Form.Select>
-                    </div>
-                  </div>
-                <div className={detalleTicketCSS.contentItem}>
-                  <Form.Label className={detalleTicketCSS.label}>Fecha de creación:</Form.Label>
-                  <Form.Control
-                    className={`${(disabled) ? detalleTicketCSS.disabled : ''} ${detalleTicketCSS.input}`}
-                    type="text"
-                    id="creationDate"
-                    disabled
-                    value={ticketCurrent.creationDate}
-                    onChange={(value) => changeValue('creationDate',value)}
-                  />
-                  <FaCalendar className={`${detalleTicketCSS.icon}  ${detalleTicketCSS.calendar}`} />
-                </div>
-                <div className={detalleTicketCSS.contentItem}>
-                  <Form.Label className={detalleTicketCSS.label}>Fecha de Resolución:</Form.Label>
-                  <Form.Control
-                    className={`${(disabled) ? detalleTicketCSS.disabled : ''} ${detalleTicketCSS.input}`}
-                    type="text"
-                    id="resolution"
-                    disabled={disabled}
-                    value={ticketCurrent.resolution}
-                    onChange={(value)=>changeValue('resolution',value)}
-                  />
-                  <FaCalendar className={`${detalleTicketCSS.icon}  ${detalleTicketCSS.calendar}`} />
-                </div>
-                <div className={detalleTicketCSS.contentItem}>
-                  <Form.Label className={detalleTicketCSS.label}>Dias de faltantes:</Form.Label>
-                  <div className={detalleTicketCSS.contentInput}>
-                    <Form.Control
-                      className={`${(disabled) ? detalleTicketCSS.disabled : ''} ${detalleTicketCSS.input} ${detalleTicketCSS.addRight}`}
-                      type="text"
-                      id="diasRestantes"
-                      disabled
-                      value={diasRestantes}
-                    />
-                  </div>
-                </div> 
-          </Row>
-          <Row>
-           <Col className={`${detalleTicketCSS.content_iconState}`}>
-                  {ticketCurrent.type === 'ERROR' ? <MdOutlineError className={`${detalleTicketCSS.icon} 
-                  ${detalleTicketCSS.error} ${detalleTicketCSS.iconState}`} /> :
-                    ticketCurrent.type === 'CONSULTA' ?
-                      <FaQuestionCircle className={`${detalleTicketCSS.icon}  ${detalleTicketCSS.consulta} 
-                      ${detalleTicketCSS.iconState}`} /> :
-                      <MdTipsAndUpdates className={`${detalleTicketCSS.icon}  ${detalleTicketCSS.update} 
-                      ${detalleTicketCSS.iconState}`} />}
-                </Col>
-                <Col className={`${detalleTicketCSS.content_state}`}>
-                  <div className={detalleTicketCSS.contentItem}>
-                    <Form.Label className={detalleTicketCSS.label}>Severidad:</Form.Label>
-                    <div className={detalleTicketCSS.contentInput}>
-                      <Form.Select value={ticketCurrent.severity} disabled={disabled} 
-                      className={`${(disabled) ? detalleTicketCSS.disabled : ''} 
-                      ${detalleTicketCSS.input} ${detalleTicketCSS.addRightSelect}`} onChange={(value) => {changeValueSeverities(value); changeValue('severity',value)}}>
-                        {severities.map((severitie: any, index: number) => <option key={index} value={severitie.level}>{severitie.level}-{severitie.days} dias</option>)}
-                      </Form.Select>
-                    </div>
-                  </div>
-                  <div className={detalleTicketCSS.contentItem}>
-                    <Form.Label className={detalleTicketCSS.label}>Tipo:</Form.Label>
-                    <div className={detalleTicketCSS.contentInput}>
-                      <Form.Select value={ticketCurrent.type} disabled className={`
-                      ${detalleTicketCSS.input} ${detalleTicketCSS.addRightSelect} ${detalleTicketCSS.disabled}`} onChange={(value) => changeValue('type',value)}>
-                        {typesTickets.map((type: any, index: number) => <option key={index} value={type}>{type}</option>)}
-                      </Form.Select>
-                    </div>
-                  </div>
-                  <div className={detalleTicketCSS.contentItem}>
-                    <Form.Label className={detalleTicketCSS.label}>Estado:</Form.Label>
-                    <div className={detalleTicketCSS.contentInput}>
-                      <Form.Select value={ticketCurrent.status} disabled={disabled} className={`${(disabled) ? detalleTicketCSS.disabled : ''} 
-                      ${detalleTicketCSS.input} ${detalleTicketCSS.addRightSelect}`} onChange={(value) => changeValue('status',value)}>
-                        {statusTickets.map((type: any, index: number) => <option key={index} value={type}>{type}</option>)}
-                      </Form.Select>
-                    </div>
-                  </div>
-                </Col> 
-          </Row>
-        </Col>
-        <Col className={detalleTicketCSS.col8} md={6}>
-          <Row>{!disabled && <>
-            <Button className={detalleTicketCSS.iconSave} onClick={() => updateData()} variant="success">Guardar</Button>
-            <MdHighlightOff className={`${detalleTicketCSS.editIcon} ${detalleTicketCSS.iconClose}`} onClick={() => changeStateEdit(true)} />
-            </>
-          }
-          {disabled && <FaEdit className={`${detalleTicketCSS.editIcon}`} onClick={() => changeStateEdit(false)} />}
-          </Row>
-          <div>
-                  <Form.Label className={detalleTicketCSS.label} htmlFor="title">Título:</Form.Label>
-                  <Form.Control
-                    className={`${(disabled) ? (detalleTicketCSS.disabled&&detalleTicketCSS.removeCorner) : ''}`}
-                    as="textarea"
-                    rows={2}
-                    id="title"
-                    disabled={disabled}
-                    value={ticketCurrent.title}
-                    onChange={(value) => changeValue('title',value)}
-                  />
-                </div>
-                <div>
-                  <Form.Label className={detalleTicketCSS.label} htmlFor="inputPassword5">Descripción:</Form.Label>
-                  <Form.Control
-                    className={`${(disabled) ? (detalleTicketCSS.disabled&&detalleTicketCSS.removeCorner) : ''}`}
-                    as="textarea"
-                    id="description"
-                    rows={5}
-                    disabled={disabled}
-                    value={ticketCurrent.description}
-                    onChange={(value) => changeValue('description',value)}
-                  />
-                </div>
-        </Col>
-      </Row> 
-            </Container> */}
+            {task}
             {ticket.type !== 'CONSULTA' && <div className={`${detalleTicketCSS.contentTaskTickets} ${(task && task.length === 0) ? detalleTicketCSS.uninformation : ''}`}>
                 {(task && task.length > 0) && <div><Table responsive bordered >
                     <thead>
