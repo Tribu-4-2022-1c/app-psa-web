8f1535605b8ece400938682c1e9c9bc1eb919f1e
melanie <mmombru@fi.uba.ar>
2022-07-04 13:22:40 -0300

    fix modificiones y delete

--- src/Components/Recursos/TareaEmpleado.tsx
+++ src/Components/Recursos/TareaEmpleado.tsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 
 import {TareasEmpleados} from "./TareasEmpleados";
 import ProductoSoporteCSS from "../../Styles/ProductoSoporte.module.css";
-import {FaCalendar, FaFolderOpen, FaTrash} from "react-icons/fa";
+import {FaCalendar, FaEdit, FaFolderOpen, FaTrash} from "react-icons/fa";
 import versionSoporteStyle from "../../Styles/VersionSoporte.module.css";
 import {Button, Col, Form, Modal, Row} from "react-bootstrap";
 import detalleProjectCSS from "../../Styles/Proyectos/Detalle.module.css";
@@ -15,6 +15,7 @@ let idProyecto = 0;
 let idTarea = 0;
 let fechaCarga = " ";
 let numeroHorasCarga = 1;
+let defaultProject = 0;
 
 export const TareaEmpleado = (props:any) => {
     const {fechas} = props;
@@ -47,8 +48,8 @@ export const TareaEmpleado = (props:any) => {
     const [disabled, setdisabled] = useState(false);
     const [elementosVacios, setElementosVacios] = useState(false);
     const [typesTask, setTask] = useState([]);
-    const [typesProject, setProjects] = useState([]);
-      
+    const [typesProject, setProjects] = useState([]); 
+
     useEffect(() => {
         const recursos_ = async () =>{
           const allProjects:any =  await ProyectoService().getAllProjects()
@@ -102,7 +103,7 @@ export const TareaEmpleado = (props:any) => {
         
     const number_hours = [ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
 
-    const handleSubmit = async (e: { preventDefault: () => void; }) =>{
+    const handleSubmitModify = async (e: { preventDefault: () => void; }) =>{
         e.preventDefault()
         console.log("carga actual: ", cargaActual)
         let hours: HoursData = {
@@ -122,13 +123,17 @@ export const TareaEmpleado = (props:any) => {
         }
         handleClose();
     }
-
     
 
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    const [showModify, setShowModify] = useState(false);
+
+    const handleCloseModify = () => setShowModify(false);
+    const handleShowModify = () => setShowModify(true);
 
     const modifyHour = (fechas:any) => {
         console.log("modify: ", fechas['code']);
@@ -140,7 +145,8 @@ export const TareaEmpleado = (props:any) => {
         cargaInicial.code_project = fechas['code_project'];
         cargaInicial.code_employee = fechas['code_employee'];
         setCargaActual(cargaInicial);
-        handleShow();
+        defaultProject = typesTask.findIndex((type: any, index: number) => fechas['code_project'].toString().includes(type['id']));
+        handleShowModify();
     }
 
     const deleteHour = (fechas:any) => {
@@ -149,14 +155,14 @@ export const TareaEmpleado = (props:any) => {
         handleShow();
     }
 
-    // const handleSubmit = async (e: { preventDefault: () => void; }) => {
-    //     e.preventDefault()
-    //     const response = await RecursosService().deleteHours(idHourCurrent);
-    //     if (response) {
-    //         window.location.reload();
-    //     }
-    //     handleClose();
-    // }
+    const handleSubmit = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault()
+        const response = await RecursosService().deleteHours(idHourCurrent);
+        if (response) {
+            window.location.reload();
+        }
+        handleClose();
+    }
 
     return (
         <>
@@ -165,16 +171,16 @@ export const TareaEmpleado = (props:any) => {
                     {fechas.map( (fecha:any,index:number) =>
                         <div className={versionSoporteStyle.contentDescription} key={index}>{diafecha(fecha) == diaActual?<TareasEmpleados fecha={fecha["date"]} horas={fecha["number_hours"]} id={fecha["code_task"]} /> :null}
                             <div className={versionSoporteStyle.contentIcon}  
-                                onClick={() => modifyHour(fecha)}>{diafecha(fecha) == diaActual? <FaFolderOpen />:null }
+                                onClick={() => modifyHour(fecha)}>{diafecha(fecha) == diaActual? <FaEdit />:null }
                             </div>
-                            {/* <div className={versionSoporteStyle.contentIcon}  
+                            <div className={versionSoporteStyle.contentIcon}  
                                 onClick={() => deleteHour(fecha)}>{diafecha(fecha) == diaActual? <FaTrash />:null }
-                            </div> */}
+                            </div>
                         </div>
                     )}
                 </div>}
             </div>
-            {/* <Modal
+            <Modal
                 show={show}
                 onHide={handleClose}
                 backdrop="static"
@@ -199,80 +205,80 @@ export const TareaEmpleado = (props:any) => {
                         Aceptar
                     </Button>
                 </Modal.Footer>
-            </Modal> */}
+            </Modal>
 
             <Modal
-                        show={show}
-                        onHide={handleClose}
-                        backdrop="static"
-                        keyboard={false}
-                        size="lg"
-                        aria-labelledby="contained-modal-title-vcenter"
-                        centered
-                    >
-                        <Modal.Header closeButton>
-                        <Modal.Title id="contained-modal-title-vcenter">
-                            Modificar horas trabajadas
-                        </Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                        <h4>Ingrese los datos</h4>
-                        <form onSubmit={handleSubmit}>
-                        <Row>
-                            <Col className={detalleProjectCSS.col8} md={8} lg={9} m={6}>
-                                <div className={detalleProjectCSS.contentItem}>
-                                <Form.Label className={detalleProjectCSS.label}>Fecha trabajada:</Form.Label>
+                show={showModify}
+                onHide={handleCloseModify}
+                backdrop="static"
+                keyboard={false}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Modificar horas trabajadas
+                </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                <h4>Ingrese los datos</h4>
+                <form onSubmit={handleSubmit}>
+                <Row>
+                    <Col className={detalleProjectCSS.col8} md={8} lg={9} m={6}>
+                        <div className={detalleProjectCSS.contentItem}>
+                        <Form.Label className={detalleProjectCSS.label}>Fecha trabajada:</Form.Label>
 
-                                <FaCalendar className={`${detalleProjectCSS.icon}  ${detalleProjectCSS.calendar}`} />
-                                <Form.Control
-                                    className={`${(disabled) ? detalleProjectCSS.disabled : ''} ${detalleProjectCSS.input}`}
-                                    type="text"
-                                    id="date"
-                                    disabled = {false}
-                                    defaultValue={cargaActual.date}
-                                    onChange={(value) => changeFecha('date', value)}
-                                />
-                                </div>
-                                <div className={detalleProjectCSS.contentItem}>
-                                <Form.Label className={detalleProjectCSS.label}>Proyecto:</Form.Label>
-                                <div className={detalleProjectCSS.contentInput}>
-                                    <Form.Select value={detalleProjectCSS.type} disabled={disabled} className={` 
-                                        ${detalleProjectCSS.input} ${detalleProjectCSS.addRightSelect}`} defaultValue = {typesProject[0]} onChange={(e) => changeProyecto("type",e)}>
-                                    {typesProject.map((type: any, index: number) => <option key={index} value={type['id']} >{type['nombre']}</option>)}
-                                    </Form.Select>
-                                </div>
-                                </div>
-                                <div className={detalleProjectCSS.contentItem}>
-                                <Form.Label className={detalleProjectCSS.label}>Tarea:</Form.Label>
-                                <div className={detalleProjectCSS.contentInput}>
-                                    <Form.Select value={detalleProjectCSS.type} disabled={disabled} className={` 
-                                        ${detalleProjectCSS.input} ${detalleProjectCSS.addRightSelect}`}  onChange={(e) => changeTarea("task",e)}>
-                                        {typesTask.map((type: any, index: number) => <option key={index} value={type['id']}>{type['nombre']}</option>)}
-                                    </Form.Select>
-                                </div>
-                                </div>
-                                <div className={detalleProjectCSS.contentItem}>
-                                <Form.Label className={detalleProjectCSS.label}>Cantidad de horas:</Form.Label>
-                                <div className={detalleProjectCSS.contentInput}>
-                                    <Form.Select value={detalleProjectCSS.type} disabled={disabled} className={` 
-                                        ${detalleProjectCSS.input} ${detalleProjectCSS.addRightSelect}`} defaultValue = {cargaActual.number_hours} onChange={(e) => changeHoras("numbers_hours",e)}>
-                                        {number_hours.map((type: any, index: number) => <option key={index} value={type}>{type}</option>)}
-                                    </Form.Select>
-                                </div>
-                                </div>
-                            </Col>
-                        </Row>
-                        </form>
-                        </Modal.Body>
-                        <Modal.Footer>
-                        <Button variant="danger" onClick={handleClose}>
-                            Cancelar
-                        </Button>
-                        <Button variant="primary" type="submit" onClick={handleSubmit}>
-                            Guardar Cambios
-                        </Button>
-                        </Modal.Footer>
-                    </Modal>
+                        <FaCalendar className={`${detalleProjectCSS.icon}  ${detalleProjectCSS.calendar}`} />
+                        <Form.Control
+                            className={`${(disabled) ? detalleProjectCSS.disabled : ''} ${detalleProjectCSS.input}`}
+                            type="text"
+                            id="date"
+                            disabled = {false}
+                            defaultValue={cargaActual.date}
+                            onChange={(value) => changeFecha('date', value)}
+                        />
+                        </div>
+                        <div className={detalleProjectCSS.contentItem}>
+                        <Form.Label className={detalleProjectCSS.label}>Proyecto:</Form.Label>
+                        <div className={detalleProjectCSS.contentInput}>
+                            <Form.Select value={detalleProjectCSS.type} disabled={disabled} className={` 
+                                ${detalleProjectCSS.input} ${detalleProjectCSS.addRightSelect}`} defaultValue = {typesProject[0]} onChange={(e) => changeProyecto("type",e)}>
+                            {typesProject.map((type: any, index: number) => <option key={index} value={type['id']} >{type['nombre']}</option>)}
+                            </Form.Select>
+                        </div>
+                        </div>
+                        <div className={detalleProjectCSS.contentItem}>
+                        <Form.Label className={detalleProjectCSS.label}>Tarea:</Form.Label>
+                        <div className={detalleProjectCSS.contentInput}>
+                            <Form.Select value={detalleProjectCSS.type} disabled={disabled} className={` 
+                                ${detalleProjectCSS.input} ${detalleProjectCSS.addRightSelect}`}  onChange={(e) => changeTarea("task",e)}>
+                                {typesTask.map((type: any, index: number) => <option key={index} value={type['id']}>{type['nombre']}</option>)}
+                            </Form.Select>
+                        </div>
+                        </div>
+                        <div className={detalleProjectCSS.contentItem}>
+                        <Form.Label className={detalleProjectCSS.label}>Cantidad de horas:</Form.Label>
+                        <div className={detalleProjectCSS.contentInput}>
+                            <Form.Select value={detalleProjectCSS.type} disabled={disabled} className={` 
+                                ${detalleProjectCSS.input} ${detalleProjectCSS.addRightSelect}`} defaultValue = {cargaActual.number_hours} onChange={(e) => changeHoras("numbers_hours",e)}>
+                                {number_hours.map((type: any, index: number) => <option key={index} value={type}>{type}</option>)}
+                            </Form.Select>
+                        </div>
+                        </div>
+                    </Col>
+                </Row>
+                </form>
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant="danger" onClick={handleCloseModify}>
+                    Cancelar
+                </Button>
+                <Button variant="primary" type="submit" onClick={handleSubmitModify}>
+                    Guardar Cambios
+                </Button>
+                </Modal.Footer>
+            </Modal>
         </>
     )
 
