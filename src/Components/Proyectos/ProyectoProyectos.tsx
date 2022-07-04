af2900a59acfa34a39be423f08cfd6c4820fab7b
gutierrezmatias <matiasgutierrez@outlook.com>
2022-07-04 10:06:15 -0300

    fix de formato

--- src/Components/Proyectos/ProyectoProyectos.tsx
+++ src/Components/Proyectos/ProyectoProyectos.tsx
@@ -201,8 +201,7 @@ export const ProyectoProyectos = (props: any) => {
 
         </form>
       </div>
-      <Row className={detalleProjectCSS.col8} md={40} lg={40} m={40}>
-        <Col className={detalleProjectCSS.col8} md={20} lg={20} m={20}>
+
           <div className={`${detalleProjectCSS.contentTaskprojects} ${(tareas&&tareas.length===0) ? detalleProjectCSS.uninformation : ''}`}>
             {((tareas)&& !(Object.keys(tareas).length === 0)) && <Table responsive bordered >
               <thead>
@@ -238,8 +237,7 @@ export const ProyectoProyectos = (props: any) => {
               </Card>
             }
           </div>
-        </Col>
-      </Row>
+
     </div>
   )
   
