79c3d67d3d4dedc89d4a13eb44de020d83e4fcb5
BrunoMorse <brunomorseletto@gmail.com>
2022-07-06 15:15:25 -0300

    se puede retroceder en el calendario y avazar hasta la semana de la fecha actual

--- src/Components/Recursos/Calendario.tsx
+++ src/Components/Recursos/Calendario.tsx
@@ -10,10 +10,13 @@ import recursosCSS from "../../Styles/Recursos/Recursos.module.css";
 import {NavLink} from "react-router-dom";
 import versionSoporteStyle from "../../Styles/VersionSoporte.module.css";
 import {render} from "react-dom";
+import {FaArrowLeft, FaArrowRight, FaEdit} from "react-icons/fa";
+import {Empleados} from "./Empleados";
 
 
 export const Calendario = (props:any) => {
     let dia_hoy=new Date();
+    let semana_hoy = new Date();
     var day = dia_hoy.getDay(),
         diff = dia_hoy.getDate() - day + (day == 0 ? -6:1);
     let [semana, setSemana] = useState(new Date(dia_hoy.setDate(diff)));
@@ -60,12 +63,21 @@ export const Calendario = (props:any) => {
     }
 
     function changeDate() {
-       semana.setDate(semana.getDate()+7)
-        setSemanaString(semana.getFullYear() + '-' + (semana.getMonth() + 1) + '-' + semana.getDate())
-        setSemana(semana)
+        semana.setDate(semana.getDate()-7);
+        setSemanaString(semana.getFullYear() + '-' + (semana.getMonth() + 1) + '-' + semana.getDate());
+        setSemana(semana);
     }
 
 
+    function changeDateFoward() {
+        semana.setDate(semana.getDate()+7);
+        if(semana_hoy<semana){
+            semana.setDate((new Date(semana_hoy).getDate()) - day + (day == 0 ? -6:1))
+        }else{
+            setSemanaString(semana.getFullYear() + '-' + (semana.getMonth() + 1) + '-' + semana.getDate())
+            setSemana(semana)}
+
+    }
 
     return (
         <div>
@@ -91,19 +103,23 @@ export const Calendario = (props:any) => {
                         <NavLink
                             to={'/recursos/calendario'}
                             className={versionSoporteStyle.styleNav}>
-                        <div className={recursosCSS.button} onClick={() => {changeDate();}} >
-                        <p>{semanastring}</p>
+                            <div className={recursosCSS.button} onClick={() => {changeDate();}} >
+                                <FaArrowLeft />
+                            </div>
+                            <div className={recursosCSS.button}  >
+                                <p>{semanastring}</p>
 
                             </div>
-
+                            <div className={recursosCSS.button} onClick={() => {changeDateFoward();}} >
+                                <FaArrowRight />
+                            </div>
                         </NavLink>
                     </div>
-
-                    {!load&&dias.map((dias,index) => <div key={index}>
-                        <CalendarioDias dia={dias} semana={semana}/>
-
-                    </div>)}
                 </div >
+                {!load&&dias.map((dias,index) => <div key={index}>
+                    <CalendarioDias dia={dias} semana={semana}/>
+                </div>)}
+
             </div>
         </div>
     )
--- src/Components/Recursos/CalendarioDias.tsx
+++ src/Components/Recursos/CalendarioDias.tsx
@@ -3,7 +3,7 @@ import {TareaEmpleado} from "./TareaEmpleado";
 import React, { useEffect, useState } from 'react'
 import ProductoSoporteCSS from  '../../Styles/ProductoSoporte.module.css'//'../ Soporte/'
 import {FaCalendar, FaPlusCircle, FaTrash} from 'react-icons/fa'
-import { TareasEmpleados } from './TareasEmpleados'; 
+import { TareasEmpleados } from './TareasEmpleados';
 import { Button, Col, Form, Modal, Row } from 'react-bootstrap';
 import detalleProjectCSS from '../../Styles/Proyectos/Detalle.module.css';
 import { Hours, HoursData } from '../../models/Recursos.models';
@@ -11,6 +11,7 @@ import RecursosService from '../../Services/recursosService';
 import ProyectoService from '../../Services/proyectosService';
 import {ProyectoSinLider} from "../../models/Proyectos.models";
 import recursosService from "../../Services/recursosService";
+import recursosCSS from "../../Styles/Recursos/Recursos.module.css";
 
 let idProyecto = 0;
 let idTarea = 0;
@@ -71,28 +72,28 @@ const CalendarioDias = (props:any) => {
                 setASignarProjecto(false)
             }
         }
-      }
+    }
 
 
-      
+
     useEffect(() => {
         const recursos_ = async () =>{
-          const allProjects:any =  await ProyectoService().getAllProjects()
-          setProjects(allProjects);
-          idProyecto = allProjects[0].id;
-          const recursos_ = async () =>{
-              const allTasks: any = await ProyectoService().getTaskForProject(idProyecto.toString())
-              setTask(allTasks);
-              if(allTasks.length == 0) {
-                  idTarea = 0;
-              } else {
-                  idTarea = allTasks[0].id;
-              }
-          }
-          recursos_();
+            const allProjects:any =  await ProyectoService().getAllProjects()
+            setProjects(allProjects);
+            idProyecto = allProjects[0].id;
+            const recursos_ = async () =>{
+                const allTasks: any = await ProyectoService().getTaskForProject(idProyecto.toString())
+                setTask(allTasks);
+                if(allTasks.length == 0) {
+                    idTarea = 0;
+                } else {
+                    idTarea = allTasks[0].id;
+                }
+            }
+            recursos_();
         }
         recursos_();
-      },[]);
+    },[]);
 
 
     useEffect(() => {
@@ -129,7 +130,7 @@ const CalendarioDias = (props:any) => {
     const changeHoras = (prop: string, value: any) => {
         numeroHorasCarga = value.target.value;
     }
-        
+
     const number_hours = [ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
 
     const handleSubmit = async (e: { preventDefault: () => void; }) =>{
@@ -169,10 +170,9 @@ const CalendarioDias = (props:any) => {
     let finsemana = new Date(diainicial.setDate(diainicial.getDate()+6))
     let mesfinsemana;
     let diafinsemana;
-
     useEffect(() => {
         const tasks_ = async () =>{
-            console.log(finsemana);
+            console.log(semana);
             ((semana.getMonth() + 1) < 10 ? mes = "0" + (semana.getMonth() + 1) : mes = semana.getMonth() + 1);
             ((semana.getDate()) < 10 ? numerodia = "0" + (semana.getDate()) : numerodia = semana.getDate());
             ((finsemana.getMonth() + 1) < 10 ? mesfinsemana = "0" + (finsemana.getMonth() + 1) : mesfinsemana = finsemana.getMonth() + 1);
@@ -184,109 +184,109 @@ const CalendarioDias = (props:any) => {
             setload(false);
         }
         tasks_();
-    },[]);
+    },[props]);
 
 
     return (
-        < >
-        <div>
-            <div
-                className={`${ProductoSoporteCSS.card} 
+        <div >
+            <div>
+                <div
+                    className={`${ProductoSoporteCSS.card} 
         ${(diaSelect&&dia&&diaSelect.id===dia.id)?ProductoSoporteCSS.isSelected:''}`}>
-                <div className={ProductoSoporteCSS.contentDescription}>
-                    <p>{dia['dia']} </p>
+                    <div className={ProductoSoporteCSS.contentDescription}>
+                        <p>{dia['dia']} </p>
+                    </div>
+                    <div className={ProductoSoporteCSS.contentIcon} onClick={handleShow}>
+                        <FaPlusCircle  />
+                    </div>
                 </div>
-                <div className={ProductoSoporteCSS.contentIcon} onClick={handleShow}>
-                    <FaPlusCircle  />
-                </div>
-            </div>
-            <div
-                className={`${ProductoSoporteCSS.contentDescription} 
+                <div
+                    className={`${ProductoSoporteCSS.contentDescription} 
         ${(diaSelect&&dia&&diaSelect.id===dia.id)?ProductoSoporteCSS.isSelected:''}`}>
                     <TareaEmpleado fechas={allhoras} diaActual={dia['dia']} horas={allhoras} currentHour={allhoras[0]}  />
+                </div>
             </div>
-        </div>
-                    <Modal
-                        show={show}
-                        onHide={handleClose}
-                        backdrop="static"
-                        keyboard={false}
-                        size="lg"
-                        aria-labelledby="contained-modal-title-vcenter"
-                        centered
-                    >
-                        <Modal.Header closeButton>
-                        <Modal.Title id="contained-modal-title-vcenter">
-                            Cargar horas trabajadas
-                        </Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                        <h4>Ingrese los datos</h4>
-                        <form onSubmit={handleSubmit}>
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Cargar horas trabajadas
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <h4>Ingrese los datos</h4>
+                    <form onSubmit={handleSubmit}>
                         <Row>
                             <Col className={detalleProjectCSS.col8} md={8} lg={9} m={6}>
                                 <div className={detalleProjectCSS.contentItem}>
-                                <Form.Label className={detalleProjectCSS.label}>Fecha trabajada:</Form.Label>
+                                    <Form.Label className={detalleProjectCSS.label}>Fecha trabajada:</Form.Label>
 
-                                <FaCalendar className={`${detalleProjectCSS.icon}  ${detalleProjectCSS.calendar}`} />
-                                <Form.Control
-                                    className={`${(disabled) ? detalleProjectCSS.disabled : ''} ${detalleProjectCSS.input}`}
-                                    type="text"
-                                    id="date"
-                                    disabled = {false}
-                                    defaultValue={cargaActual.date}
-                                    onChange={(value) => changeFecha('date', value)}
-                                />
+                                    <FaCalendar className={`${detalleProjectCSS.icon}  ${detalleProjectCSS.calendar}`} />
+                                    <Form.Control
+                                        className={`${(disabled) ? detalleProjectCSS.disabled : ''} ${detalleProjectCSS.input}`}
+                                        type="text"
+                                        id="date"
+                                        disabled = {false}
+                                        defaultValue={cargaActual.date}
+                                        onChange={(value) => changeFecha('date', value)}
+                                    />
                                 </div>
                                 <div className={detalleProjectCSS.contentItem}>
-                                <Form.Label className={detalleProjectCSS.label}>Proyecto:</Form.Label>
-                                <div className={detalleProjectCSS.contentInput}>
-                                    <Form.Select value={detalleProjectCSS.type} disabled={disabled} className={` 
+                                    <Form.Label className={detalleProjectCSS.label}>Proyecto:</Form.Label>
+                                    <div className={detalleProjectCSS.contentInput}>
+                                        <Form.Select value={detalleProjectCSS.type} disabled={disabled} className={` 
                                         ${detalleProjectCSS.input} ${detalleProjectCSS.addRightSelect}`} defaultValue = {typesProject[0]} onChange={(e) => changeProyecto("type",e)}>
-                                    {typesProject.map((type: any, index: number) => <option key={index} value={type['id']} >{type['nombre']}</option>)}
-                                    </Form.Select>
-                                </div>
+                                            {typesProject.map((type: any, index: number) => <option key={index} value={type['id']} >{type['nombre']}</option>)}
+                                        </Form.Select>
+                                    </div>
                                 </div>
                                 <div className={detalleProjectCSS.contentItem}>
-                                <Form.Label className={detalleProjectCSS.label}>Tarea:</Form.Label>
-                                <div className={detalleProjectCSS.contentInput}>
-                                    <Form.Select value={detalleProjectCSS.type} disabled={disabled} className={` 
+                                    <Form.Label className={detalleProjectCSS.label}>Tarea:</Form.Label>
+                                    <div className={detalleProjectCSS.contentInput}>
+                                        <Form.Select value={detalleProjectCSS.type} disabled={disabled} className={` 
                                         ${detalleProjectCSS.input} ${detalleProjectCSS.addRightSelect}`}  onChange={(e) => changeTarea("task",e)}>
-                                        {typesTask.map((type: any, index: number) => <option key={index} value={type['id']}>{type['nombre']}</option>)}
-                                    </Form.Select>
-                                </div>
+                                            {typesTask.map((type: any, index: number) => <option key={index} value={type['id']}>{type['nombre']}</option>)}
+                                        </Form.Select>
+                                    </div>
                                 </div>
                                 <div className={detalleProjectCSS.contentItem}>
-                                <Form.Label className={detalleProjectCSS.label}>Cantidad de horas:</Form.Label>
-                                <div className={detalleProjectCSS.contentInput}>
-                                    <Form.Select value={detalleProjectCSS.type} disabled={disabled} className={` 
+                                    <Form.Label className={detalleProjectCSS.label}>Cantidad de horas:</Form.Label>
+                                    <div className={detalleProjectCSS.contentInput}>
+                                        <Form.Select value={detalleProjectCSS.type} disabled={disabled} className={` 
                                         ${detalleProjectCSS.input} ${detalleProjectCSS.addRightSelect}`}  onChange={(e) => changeHoras("numbers_hours",e)}>
-                                        {number_hours.map((type: any, index: number) => <option key={index} value={type}>{type}</option>)}
-                                    </Form.Select>
-                                </div>
+                                            {number_hours.map((type: any, index: number) => <option key={index} value={type}>{type}</option>)}
+                                        </Form.Select>
+                                    </div>
                                 </div>
                             </Col>
                         </Row>
-                        </form>
-                        </Modal.Body>
-                        <Modal.Footer>
-                        <Button variant="danger" onClick={handleClose}>
-                            Cancelar
-                        </Button>
-                        <Button variant="primary" type="submit" onClick={handleSubmit}>
-                            Guardar Cambios
-                        </Button>
-                        {elementosVacios &&
+                    </form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="danger" onClick={handleClose}>
+                        Cancelar
+                    </Button>
+                    <Button variant="primary" type="submit" onClick={handleSubmit}>
+                        Guardar Cambios
+                    </Button>
+                    {elementosVacios &&
                         <div>
                             Hay elementos vacios
                         </div>
-                        }
-                        </Modal.Footer>
-                    </Modal>
-        </>
+                    }
+                </Modal.Footer>
+            </Modal>
+        </div>
     )
 }
 
 
-    export default CalendarioDias;
+export default CalendarioDias;
 
